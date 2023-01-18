81799380cec206cca672070efb148fa5e7ef45d4
GitLukeW <gitlukew@gmail.com>
2023-01-18 18:37:07 -0500

    netlify setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return (<h1>Hello World</h1>)
+  return <h1>Hello World</h1>
 }
 
 export default App;
