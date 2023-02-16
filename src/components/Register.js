6a415bdea96e4523c0b1e32adc17ade2ae3752d0
Emily Starr <emilyflo.starr@gmail.com>
2023-02-16 13:16:10 -0500

    registration is set up but not completely functional

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,110 @@
-// import { Navigate } from 'react-router-dom';
-// import { useState } from 'react';
-// import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import {FormControl, FormLabel, Button, Input } from '@chakra-ui/react';
 
-const Register = ({ isLoggedIn }) => {
+const Register = ({ setAuth, isLoggedIn }) => {
 
-    // const [username, setUsername] = useState('');
-    // const [password, setPassword] = useState('');
-    // const [error, setError] = useState('');
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     // const navigate = useNavigate();
-    // if (isLoggedIn) {
-    //     return <Navigate to="/sessions" />
-    // }
-    return <h1> This is a registration form </h1>
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(userName, email, password)
+
+        axios
+        .post ('https://team-production-system.onrender.com/auth/users/', {
+            username: userName,
+            password: password,
+            email: email,
+        })
+
+        .then(() =>
+        axios
+        .post('https://team-production-system.onrender.com//auth/token/login', {
+            username: userName,
+            password: password,
+        })
+    )
+    .then((res) =>
+    setAuth(userName, res.data.auth_token))
+
+    .catch((error) => {
+        if (error.response.data.userName)
+            setError(error.response.data.userName);
+
+        if (error.response.data.password)
+            setError(error.response.data.password)
+    })
+}
+    return (
+        <div>
+            <form onSubmit={handleRegister}>
+            <FormControl  className="form--registration">
+
+                <div className="field">
+                    <FormLabel className="label" htmlFor="email">
+                        E-mail
+                    </FormLabel>
+                    <div>
+                        <Input
+                            type="text"
+                            id="email"
+                            className="input"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <FormLabel className="label" htmlFor="username">
+                        Username
+                    </FormLabel>
+                    <div>
+                        <Input
+                            type="text"
+                            id="username"
+                            className="input"
+                            required
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                            />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <FormLabel className="label" htmlFor="password">
+                        Password
+                    </FormLabel>
+                    <div>
+                        <Input
+                            type="password"
+                            id="password"
+                            className="input"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <Button type="submit" to="/sessions" >
+                            Register
+                        </Button>
+                    </div>
+                </div>
+                
+            </FormControl>
+            </form>
+        </div>
+    )
 }
 
 export default Register
