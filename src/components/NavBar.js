898e6d0745b967a041b250bdfe8875581191b268
GitLukeW <gitlukew@gmail.com>
2023-02-20 21:04:54 -0500

    trying out some styling for the nav/hero

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,28 +2,29 @@ import { ButtonGroup, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const NavBar = ({ handleLogout, isLoggedIn }) => {
-    return (
-        <nav className="nav" role="navigation">
+  return (
+    <nav className="nav" role="navigation">
+      <div className="nav--title-container"> </div>
         <h1 className="nav--title">Momentum Mentors</h1>
-        <div className="nav--authentication">
-            {!isLoggedIn ? (
-                <>
-                <ButtonGroup>
-                    <Link to="/register">
-                        <Button>Sign up</Button>
-                    </Link>
-                    <Link to="/login">
-                        <Button>Log in</Button>
-                    </Link>
-                </ButtonGroup>
-                </>
-            ) : (
-                <Link to="/" onClick={handleLogout}>
-                    Log out
-                </Link>
-            )}
-        </div>
-        </nav>
-    )
-}
-export default NavBar
+     
+      <div className="nav--authentication">
+        {!isLoggedIn ? (
+          <ButtonGroup>
+            <Link to="/register">
+              <Button>Sign up</Button>
+            </Link>
+            <Link to="/login">
+              <Button>Log in</Button>
+            </Link>
+          </ButtonGroup>
+        ) : (
+          <Link to="/" onClick={handleLogout}>
+            Log out
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
