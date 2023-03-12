f2a14ac13f93f68f2cf7b5702e8fc4ef26d0ec66
GitLukeW <gitlukew@gmail.com>
2023-03-12 13:08:38 -0400

    adding a loading message to the login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,84 +1,96 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import {FormControl, FormLabel, Button, Input } from '@chakra-ui/react';
+import { FormControl, FormLabel, Button, Input } from '@chakra-ui/react';
 
 export default function Login({ setAuth }) {
-    const [userName, setUserName] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleLogin = (event) => {
-        event.preventDefault()
-        setError('')
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setError('');
+    setLoading(true);
+    axios
+      .post('https://team-production-system.onrender.com/auth/token/login/', {
+        username: userName,
+        password: password,
+      })
+      .then((res) => {
+        const token = res.data.auth_token;
         axios
-            .post('https://team-production-system.onrender.com/auth/token/login/', {
-                username: userName,
-                password: password,
-            })
-            .then((res) => {
-                const token = res.data.auth_token
-                axios
-                .get('https://team-production-system.onrender.com/myprofile/', {
-                    headers: { Authorization: `Token ${token}`, },}
-                    )
-                .then((res) => {
-                    setAuth(userName, token, res.data.pk);
-                    console.log(res.data);
-                    navigate("/profile");
-                })
-                .catch((e) => setError(e.message))
-            })
-            .catch((e) => setError(e.message))
-    }
+          .get('https://team-production-system.onrender.com/myprofile/', {
+            headers: { Authorization: `Token ${token}` },
+          })
+          .then((res) => {
+            setLoading(false);
+            setAuth(userName, token, res.data.pk);
+            console.log(res.data);
+            navigate('/profile');
+          })
+          .catch((e) => {
+            setLoading(false);
+            setError(e.message);
+          });
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e.message);
+      });
+  };
 
+  return (
+    <div className="Login">
+      <form onSubmit={handleLogin}>
+        <FormControl className="form--login">
+          <div className="field">
+            <FormLabel className="label" htmlFor="username">
+              Username
+            </FormLabel>
+            <div>
+              <Input
+                type="text"
+                id="username"
+                className="input"
+                required
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
 
-    return (
-        <div className="Login">
-            <form onSubmit={handleLogin}>
-            <FormControl  className="form--login">
-                <div className="field">
-                    <FormLabel className="label" htmlFor="username">
-                        Username
-                    </FormLabel>
-                    <div>
-                        <Input
-                            type="text"
-                            id="username"
-                            className="input"
-                            required
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            />
-                    </div>
+            <div className="field">
+              <FormLabel className="label" htmlFor="password">
+                Password
+              </FormLabel>
+              <div>
+                <Input
+                  type="password"
+                  id="password"
+                  className="input"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
 
-                <div className="field">
-                    <FormLabel className="label" htmlFor="password">
-                        Password
-                    </FormLabel>
-                    <div>
-                        <Input
-                            type="password"
-                            id="password"
-                            className="input"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="button--login">
-                        <Button type="submit" to="/profile" >
-                            Log in
-                        </Button>
-                    </div>
-                </div>
-                </div>
-            </FormControl>
-            </form>
-        </div>
-    )
+            <div>
+              <div className="button--login">
+                {loading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <Button type="submit" to="/profile">
+                    Log in
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
+        </FormControl>
+      </form>
+    </div>
+  );
 }
