5b1f3ea884e7801f5bfa023195d30b29e85ac30f
CapelHoworth <capelhoworth@gmail.com>
2023-03-28 16:35:06 -0400

    mentor page formatting

--- src/components/ProfileBasicInfo.js
+++ src/components/ProfileBasicInfo.js
@@ -1,23 +1,22 @@
-import { Link } from 'react-router-dom';
-import { Button } from '@chakra-ui/react';
-import { Box, Heading } from '@chakra-ui/react';
+import { useNavigate } from "react-router-dom";
+import { Button, Grid, Typography } from "@mui/material";
 
-export default function ProfileBasicInfo ({ firstName, lastName, phoneNumber }) {
-    // We can set this component up to include different info depending on what we pass in (for instance the wireframes currently indicate that students will have a team # and mentors will not)
+export default function ProfileBasicInfo({ firstName, lastName, phoneNumber }) {
+  // We can set this component up to include different info depending on what we pass in (for instance the wireframes currently indicate that students will have a team # and mentors will not)
+  const navigate = useNavigate();
 
-    return(
-        <Box>
-            <Heading className="profile--heading">
-                <img src="" alt=""/>
-                <p>{firstName} {lastName}</p>
-                <p>{phoneNumber}</p>
-                
-                <Link to="/editprofile" >
-                    <Button
-                    border='2px'
-                    borderColor='orange.200'>Edit</Button>
-                </Link>
-            </Heading>
-        </Box>
-    )
+  return (
+    <Grid container alignItems="center" justifyContent="center">
+      <Grid item className="profile--heading">
+        <img src="" alt="" />
+        <Typography>
+          {firstName} {lastName}
+        </Typography>
+        <p>{phoneNumber}</p>
+        <Button variant="outlined" onClick={() => navigate("/editprofile")}>
+          Edit
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
