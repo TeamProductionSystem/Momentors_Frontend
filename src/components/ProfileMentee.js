5375a5378b9177ceab197775696465bd82cb8416
GitLukeW <gitlukew@gmail.com>
2023-04-03 18:09:01 -0400

    formated the mentee profile page

--- src/components/ProfileMentee.js
+++ src/components/ProfileMentee.js
@@ -1,38 +1,39 @@
-import { useState, useEffect } from 'react';
-import { Image, Switch, FormControl, FormLabel } from '@chakra-ui/react';
-import axios from 'axios';
-import ProfileBasicInfo from './ProfileBasicInfo';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Grid, Avatar, Typography } from "@mui/material";
 
-export default function ProfileMentee ({ token, pk, setAuth }) {
 
-// first name, last name, phone number
-const [firstName, setFirstName] = useState("");
-const [lastName, setLastName] = useState("");
-const [phoneNumber, setPhoneNumber] = useState("");
+export default function ProfileMentee({ token, pk, setAuth }) {
+  // first name, last name, phone number
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     axios
-        .get(`https://team-production-system.onrender.com/myprofile/`, {
-            headers: { Authorization: `Token ${token}` },
-        })
-        .then((res) => {
-            setFirstName(res.data.first_name);
-            setLastName(res.data.last_name);
-            setPhoneNumber(res.data.phone_number);
-            console.log(res)
-        })
-}, [token, pk])
-            
-    return (
-        <div className="profile--page">
-        <ProfileBasicInfo firstName={firstName} lastName={lastName} phoneNumber={phoneNumber} />
-        <FormControl display='flex' alignItems='center'>
-            <FormLabel className="authentication-switch">
-            Mentor or Mentee?
-            </FormLabel>
-            <Switch id='switch--mentor-mentee' />
-        </FormControl>
-        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZfWLr5vMAESMl38Yncgk4rl7y3-_OiD9nnprz8SGTj2ClL1NfEsn46eXLiPO82dWXZuk&usqp=CAU" alt="temporary image"></Image>
-        </div>
-    )
+      .get(`https://team-production-system.onrender.com/myprofile/`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => {
+        setFirstName(res.data.first_name.charAt(0).toUpperCase() + res.data.first_name.slice(1));
+        setLastName(res.data.last_name.charAt(0).toUpperCase() + res.data.last_name.slice(1));
+        setPhoneNumber(res.data.phone_number.slice(2).replace(/(\d{3})(\d{3})(\d{4})/, '($1)-$2-$3')); // format phone number
+        setProfilePhoto(res.data.profile_photo); // set profilePhoto state
+      });
+  }, [token, pk]);
+
+  return (
+    <div className="profile--page" style={{ marginTop: "50px" }} >
+      <Grid container spacing={2}>
+        <Grid item>
+          <Avatar sx={{ width: 200, height: 210 }} src={profilePhoto} />
+        </Grid>
+        <Grid item>
+          <Typography variant="h4">{`UserName: ${firstName} ${lastName}`}</Typography>
+          <Typography variant="h6">{`Phone Number: ${phoneNumber}`}</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
