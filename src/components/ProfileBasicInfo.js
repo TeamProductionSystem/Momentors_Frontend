d7925af17a1170ae9719e6417f6167ff47fed13c
CapelHoworth <capelhoworth@gmail.com>
2023-04-27 19:22:29 -0400

    started mentor profile page cleanup

--- src/components/ProfileBasicInfo.js
+++ src/components/ProfileBasicInfo.js
@@ -1,21 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography, Avatar } from "@mui/material";
 
-export default function ProfileBasicInfo({ firstName, lastName, phoneNumber, about_me, skills }) {
+export default function ProfileBasicInfo({
+  firstName,
+  lastName,
+  phoneNumber,
+  about_me,
+  skills,
+  email,
+  profilePhoto,
+}) {
   // We can set this component up to include different info depending on what we pass in (for instance the wireframes currently indicate that students will have a team # and mentors will not)
   const navigate = useNavigate();
 
   return (
-    <Grid container alignItems="center" justifyContent="center">
-      <Grid item className="profile--heading">
-        <img src="" alt="" />
+    <Grid container border="solid">
+      <Grid item xs={6}>
+        <Avatar sx={{ width: 200, height: 210 }} src={profilePhoto} />
+      </Grid>
+      <Grid item xs={6}>
         <Typography>
-          {firstName} 
+          {firstName} {lastName}
         </Typography>
-        <Typography>
-          {lastName} 
-        </Typography>
-        <p>{phoneNumber}</p>
+        <br />
+        <Typography>Email:</Typography>
+        <Typography>{email}</Typography>
+        <Typography>Phone Number:</Typography>
+        <Typography>{phoneNumber}</Typography>
         <Button variant="outlined" onClick={() => navigate("/editprofile")}>
           Edit
         </Button>
