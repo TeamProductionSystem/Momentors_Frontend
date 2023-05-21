c0a778402ecf19afd7f3571aeea4676169210336
GitLukeW <gitlukew@gmail.com>
2023-05-21 11:52:00 -0400

    Updated the styling

--- src/pages/SessionSignup/MentorCard.js
+++ src/pages/SessionSignup/MentorCard.js
@@ -1,31 +1,22 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
 
 export default function MentorCard({ mentor }) {
   return (
     <>
       <Grid container alignItems="center" justifyContent="center">
-        <Card sx={{ minWidth: 400, maxWidth: 400 }} elevation={4}>
+        <Card sx={{ minWidth: 300, maxWidth: 350, minHeight: 300}} elevation={4}>
           <CardMedia
             sx={{
-              height: 400,
+              height: 300,
             }}
             image={mentor.profile_photo}
             title="Profile Photo"
           />
           <CardContent>
-            <Typography gutterBottom>{mentor.about_me}</Typography>
+            {mentor.first_name} {mentor.last_name}
+            <Typography >Bio: {mentor.about_me}</Typography>
             {/* Pull in bio from database */}
-            <Typography>
-              Skills: {mentor.skills}
-              <br />
-              firstName:
-              {mentor.first_name}
-              <br />
-              lastName:
-              {mentor.last_name}
-            </Typography>
+            <Typography>Skills: {mentor.skills.join(', ') }</Typography>
           </CardContent>
           {/* Pull in marked skills from database */}
         </Card>
