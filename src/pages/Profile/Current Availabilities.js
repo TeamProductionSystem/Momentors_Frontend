d35b10926523f910574b9a2d78aa3f9dc5ed5235
GitLukeW <gitlukew@gmail.com>
2023-06-07 19:56:15 -0400

    working current availabilities component

--- src/pages/Profile/Current Availabilities.js
+++ src/pages/Profile/Current Availabilities.js
@@ -1,21 +1,81 @@
-// This component is the Current Availabilities page in the Profile tab.
-
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Box, Typography, Grid } from "@mui/material";
 
-export default function CurrentAvailabilities({ token, pk, setAuth }) {
-    const [availabilities, setAvailabilities] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-    
-    useEffect(() => {
-        axios
-        .get(`${process.env.REACT_APP_BE_URL}/availability/`, {
-            headers: { Authorization: `Token ${token}` },
-          })
-            .then((res) => {
-                setAvailabilities(res.data);
-                setIsLoaded(true);
-            }
-        );
-    }, [token, pk]);
+export default function CurrentAvailabilities({
+  token,
+  pk,
+  setAuth,
+  refreshAvailabilities,
+  setRefreshAvailabilities,
+}) {
+  const [availabilities, setAvailabilities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BE_URL}/availability/`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => {
+        setAvailabilities(res.data);
+        setRefreshAvailabilities(false);
+      });
+  }, [token, pk, refreshAvailabilities]);
+
+  return (
+    <Box marginTop={"2rem"}>
+      <Typography variant="h4">Current Availabilities</Typography>
+      <Box margin={"1rem"}>
+        <Grid
+          container
+          sx={{
+            flexGrow: 1,
+            marginLeft: "1rem",
+            fontSize: "25px",
+            textAlign: "center",
+          }}
+        >
+          <Grid item xs={3}>
+            <Box>Date:</Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Box>Time:</Box>
+          </Grid>
+        </Grid>
+
+        {/* Map over the availabilities array and display the date and time */}
+        {availabilities.map((availability) => (
+          <Grid
+            container
+            sx={{
+              flexGrow: 1,
+              marginLeft: "1rem",
+              fontSize: "25px",
+              textAlign: "center",
+              marginTop: "1rem",
+            }}
+          >
+            <Grid item xs={3}>
+              <Box>
+                {new Date(availability.start_time).toLocaleDateString()}
+              </Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box>
+                {new Date(availability.start_time).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}{" "}
+                -{" "}
+                {new Date(availability.end_time).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </Box>
+            </Grid>
+          </Grid>
+        ))}
+      </Box>
+    </Box>
+  );
 }
