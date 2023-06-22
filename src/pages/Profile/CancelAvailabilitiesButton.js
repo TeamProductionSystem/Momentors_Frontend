d110637f26a89f3c3a27b47138cb5de06da591b8
GitLukeW <gitlukew@gmail.com>
2023-06-22 11:52:45 -0400

    deleted console log

--- src/pages/Profile/CancelAvailabilitiesButton.js
+++ src/pages/Profile/CancelAvailabilitiesButton.js
@@ -4,9 +4,12 @@ import React from "react";
 import axios from "axios";
 import { Button } from "@mui/material";
 
-const CancelAvailabilityButton = ({ availabilityPK, token, setRefreshAvailabilities }) => {
+const CancelAvailabilityButton = ({
+  availabilityPK,
+  token,
+  setRefreshAvailabilities,
+}) => {
   const handleClick = () => {
-    console.log("availabilityPK", availabilityPK);
     axios
       .delete(
         `${process.env.REACT_APP_BE_URL}/availability/${availabilityPK}/`,
