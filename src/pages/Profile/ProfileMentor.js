0463a2273a938ab2168f1cb71c8b12a7c80b414c
GitLukeW <gitlukew@gmail.com>
2023-06-24 16:47:04 -0400

    added the confirm notification

--- src/pages/Profile/ProfileMentor.js
+++ src/pages/Profile/ProfileMentor.js
@@ -26,10 +26,11 @@ export default function ProfileMentor({ token, pk, setAuth }) {
   //Update the availabilities when the user adds a new availability
   const [refreshAvailabilities, setRefreshAvailabilities] = useState(false);
 
-    // track notification switch settings
+  // track notification switch settings
   // set up similarly to https://webdevassist.com/reactjs-materialui/material-ui-switch
   const [checkedSessionReq, setCheckedSessionReq] = useState(false);
   const [checkedSessionCanc, setCheckedSessionCanc] = useState(false);
+  const [checkedSessionConfirmed, setCheckedSessionConfirmed] = useState(false);
   const [checked15Min, setChecked15Min] = useState(false);
   const [checked60Min, setChecked60Min] = useState(false);
 
@@ -72,67 +73,92 @@ export default function ProfileMentor({ token, pk, setAuth }) {
       .then((res) => {
         setCheckedSessionReq(res.data.session_requested);
         setCheckedSessionCanc(res.data.session_canceled);
+        setCheckedSessionConfirmed(res.data.session_confirmed);
         setChecked15Min(res.data.fifteen_minute_alert);
         setChecked60Min(res.data.sixty_minute_alert);
       });
   }, [token, pk]);
 
-    // handle Switch functionality
-    const handleSessionReq = (event) => {
-      setCheckedSessionReq(event.target.checked);
-      axios
-        .patch(`${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`, {
-          session_requested: event.target.checked,
+  // handle Switch functionality
+  const handleSessionReq = (event) => {
+    setCheckedSessionReq(event.target.checked);
+    axios.patch(
+      `${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`,
+      {
+        session_requested: event.target.checked,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
         },
-        {
-          headers: {
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-    }
-  
-    const handleSessionCanc = (event) => {
-      setCheckedSessionCanc(event.target.checked);
-      axios
-        .patch(`${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`, {
-          session_canceled: event.target.checked,
+      }
+    );
+  };
+
+  const handleSessionCanc = (event) => {
+    setCheckedSessionCanc(event.target.checked);
+    axios.patch(
+      `${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`,
+      {
+        session_canceled: event.target.checked,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
         },
-        {
-          headers: {
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-    }
-  
-    const handle15Min = (event) => {
-      setChecked15Min(event.target.checked);
-      axios
-        .patch(`${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`, {
-          fifteen_minute_alert: event.target.checked,
+      }
+    );
+  };
+
+  const handle15Min = (event) => {
+    setChecked15Min(event.target.checked);
+    axios.patch(
+      `${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`,
+      {
+        fifteen_minute_alert: event.target.checked,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
         },
-        {
-          headers: {
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-    }
-  
-    const handle60Min = (event) => {
-      setChecked60Min(event.target.checked);
-      axios
-        .patch(`${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`, {
-          sixty_minute_alert: event.target.checked,
+      }
+    );
+  };
+
+  const handle60Min = (event) => {
+    setChecked60Min(event.target.checked);
+    axios.patch(
+      `${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`,
+      {
+        sixty_minute_alert: event.target.checked,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
         },
-        {
-          headers: {
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-    }
+      }
+    );
+  };
+
+  const handleSessionConfirmed = (event) => {
+    setCheckedSessionConfirmed(event.target.checked);
+    axios.patch(
+      `${process.env.REACT_APP_BE_URL}/notificationsettings/${pk}/`,
+      {
+        session_confirmed: event.target.checked,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
 
   return (
     <Box className="mentorprofile--page" margin="1rem">
@@ -157,10 +183,14 @@ export default function ProfileMentor({ token, pk, setAuth }) {
         </Box>
       </Stack>
       <Stack direction="row" marginTop={"1.5rem"}>
+        {/* Seeting the size of the "What You Know" box on 192 and 193, so that is does not grow 
+        when the user adds more availabilities */}
         <Box
           sx={{ border: "2px solid", borderRadius: "16px" }}
           width={"15rem"}
           marginRight={".5rem"}
+          maxHeight={"40rem"}
+          overflow={"auto"}
         >
           <Typography
             variant="h5"
@@ -169,6 +199,7 @@ export default function ProfileMentor({ token, pk, setAuth }) {
           >
             What You Know
           </Typography>
+
           <Box>
             <List>
               {skills.map((skill, index) => (
@@ -193,24 +224,6 @@ export default function ProfileMentor({ token, pk, setAuth }) {
             <Grid container direction={"row"} spacing={2}>
               <Grid marginRight={"rem"} marginLeft={"4rem"} width={"35rem"}>
                 <Grid item>
-                  {/* <FormControl width="1rem">
-                      <InputLabel id="demo-simple-select-label">
-                        Time
-                      </InputLabel>{" "}
-                      <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        // value={age}
-                        // label="Age"
-                        // onChange={handleChange}
-                      >
-                        <MenuItem value={10}>15 Mintues</MenuItem>
-                        <MenuItem value={20}>30 Mintues</MenuItem>
-                        <MenuItem value={45}>45 Minutes</MenuItem>
-                        <MenuItem value={60}>60 Minutes</MenuItem>
-                      </Select>
-                    </FormControl> */}
-
                   <Grid item>
                     <br></br>
                   </Grid>
@@ -218,28 +231,41 @@ export default function ProfileMentor({ token, pk, setAuth }) {
                     <Typography fontSize={"18px"} alignSelf={"center"}>
                       Notify me when a mentee schedules a session
                     </Typography>
-                    <FormControlLabel control={<Switch checked={checkedSessionReq} onChange={handleSessionReq} />} />
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={checkedSessionReq}
+                          onChange={handleSessionReq}
+                        />
+                      }
+                    />
+                  </Grid>
+                  <Grid item display={"flex"} justifyContent={"space-between"}>
+                    <Typography fontSize={"18px"} alignSelf={"center"}>
+                      Notify me when a session is confirmed
+                    </Typography>
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={checkedSessionConfirmed}
+                          onChange={handleSessionConfirmed}
+                        />
+                      }
+                    />
                   </Grid>
                   <Grid item display={"flex"} justifyContent={"space-between"}>
                     <Typography fontSize={"18px"} alignSelf={"center"}>
                       Notify me when a mentee cancels a session
                     </Typography>
-                    <FormControlLabel control={<Switch checked={checkedSessionCanc} onChange={handleSessionCanc} />} />
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={checkedSessionCanc}
+                          onChange={handleSessionCanc}
+                        />
+                      }
+                    />
                   </Grid>
-
-                  {/* Commenting out notification options until we're ready to use them */}
-                  {/* <Grid item display={"flex"} justifyContent={"space-between"}>
-                    <Typography fontSize={"18px"} alignSelf={"center"}>
-                      Notify me 15 minutes before a session
-                    </Typography>
-                    <FormControlLabel control={<Switch checked={checked15Min} onChange={handle15Min} />} />
-                  </Grid>
-                  <Grid item display={"flex"} justifyContent={"space-between"}>
-                    <Typography fontSize={"18px"} alignSelf={"center"}>
-                      Notify me 60 minutes before a session
-                    </Typography>
-                    <FormControlLabel control={<Switch checked={checked60Min} onChange={handle60Min} />} />
-                  </Grid> */}
                   <Box sx={{ marginTop: "4rem" }}></Box>
                 </Grid>
               </Grid>
@@ -251,15 +277,15 @@ export default function ProfileMentor({ token, pk, setAuth }) {
               />
             </Box>
           </Box>
+          <CurrentAvailabilities
+            token={token}
+            pk={pk}
+            setAuth={setAuth}
+            refreshAvailabilities={refreshAvailabilities}
+            setRefreshAvailabilities={setRefreshAvailabilities}
+          />
         </Box>
       </Stack>
-      <CurrentAvailabilities
-        token={token}
-        pk={pk}
-        setAuth={setAuth}
-        refreshAvailabilities={refreshAvailabilities}
-        setRefreshAvailabilities={setRefreshAvailabilities}
-      />
     </Box>
   );
 }
