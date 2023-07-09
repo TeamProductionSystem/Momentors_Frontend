886a4e0292b40c358ffc81ccf40b6791c58c4c01
GitLukeW <gitlukew@gmail.com>
2023-07-09 18:07:53 -0400

    added the register button

--- src/components/RegisterButton.jsx
+++ src/components/RegisterButton.jsx
@@ -3,7 +3,7 @@ import { Button } from "@mui/material";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import { useState } from "react";
 
-export default function LoginButton({ loading }) {
+export default function RegisterButton({ loading }) {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseOver = () => {
@@ -20,12 +20,13 @@ export default function LoginButton({ loading }) {
         size="large"
         type="submit"
         component={Link}
-        to="/login"
+        to="/register"
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
         sx={{
           width: "6.8125rem",
           height: "2.8125rem",
+          marginLeft: { xs: 1, sm: 5 },
           marginTop: { xs: 2, sm: 10 },
           alignItems: "center",
           borderRadius: "0.25rem",
@@ -33,7 +34,7 @@ export default function LoginButton({ loading }) {
           fontSize: isHovered ? '2rem' : '1rem',
         }}
       >
-        {isHovered ? '🙌' : 'Sign In'}
+        {isHovered ? '🫶' : 'Register'}
       </Button>
       {loading && <PacmanLoader size={12} color="yellow" />}
     </>
--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import { Box, Grid, Typography } from "@mui/material";
 import yoda from "../../assets/mentor-yoda.jpeg";
 import LoginButton from "../../components/LoginButton";
+import RegisterButton from "../../components/RegisterButton";
 
 export default function Hero() {
   return (
@@ -22,18 +23,26 @@ export default function Hero() {
               fontSize: { xs: "2rem", sm: "3.25rem" },
               fontWeight: "400",
               lineHeight: "116.7%",
-              mt: { xs: 2, sm: 32 },
-              ml: { xs: 1, sm: 5 },
+              mt: { xs: 2, sm: 30 },
+              ml: { xs: 2, sm: 6 },
               width: "85%",
             }}
           >
             Find a mentor to help achieve your goals and a Master you will
             become...
           </Typography>
-          <LoginButton />
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <LoginButton />
+            <RegisterButton />
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Box sx={{ mt: { xs: "2rem", sm: "12.38rem" } }}>
+          <Box
+            sx={{
+              mt: { xs: 2, sm: 25 },
+              mr: { xs: 2, sm: 6 },
+            }}
+          >
             <img
               src={yoda}
               alt="the best mentor he is yoda"
@@ -46,6 +55,26 @@ export default function Hero() {
           </Box>
         </Grid>
       </Grid>
+      <Typography
+        sx={{
+          color: "rgba(216, 216, 216, 0.90)",
+          textAlign: "center",
+          fontFamily: "Roboto",
+          fontSize: "1rem",
+          fontWeight: "400",
+          lineHeight: "150%",
+          letterSpacing: "0.00938rem",
+          marginTop: { xs: 2, sm: 15 },
+          marginLeft: { xs: 1, sm: 8 },
+          marginRight: { xs: 1, sm: 8 },
+        }}
+      >
+        ** As we continue to refine our Beta version, we recommend using Google
+        Chrome for the best experience. Please also allow for extra time, as
+        server response may be slower than usual during this period. We
+        appreciate your patience and understanding. Improved cross-plateform
+        compatibility and performance enhancements are on the way! **
+      </Typography>
     </Box>
   );
 }
