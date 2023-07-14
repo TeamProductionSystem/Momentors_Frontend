02f4c95df806071c0c1ad6ae4c08652e918f5f7c
GitLukeW <gitlukew@gmail.com>
2023-07-13 21:41:04 -0400

    moved the submit button to it's own component

---
+++ src/pages/SessionSignup/SubmitButton.jsx
@@ -0,0 +1,31 @@
+import { Button, Snackbar } from "@mui/material";
+import Alert from "@mui/material/Alert";
+
+const SubmitButton = ({ handleSubmitSession, openSnackbar, handleCloseSnackbar }) => {
+  return (
+    <>
+      <Button
+        variant="contained"
+        onClick={handleSubmitSession}
+        sx={{ justifyContent: "center", backgroundColor: "#0EC202"  }}
+      >
+        Submit Session
+      </Button>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Session Requested!
+        </Alert>
+      </Snackbar>
+    </>
+  );
+};
+
+export default SubmitButton;
