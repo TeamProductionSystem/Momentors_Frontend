9650fd501bf7900edfc5100a237e737e4d9ad040
GitLukeW <gitlukew@gmail.com>
2023-08-11 16:04:32 -0400

    working on cypress implmentation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,7 @@ describe("Login", () => {
     cy.visit("https://momentum-mentors.netlify.app/");
     cy.contains("Log in").click();
     cy.url().should("include", "/login");
-    cy.get('input[username="username"]').type("test");
-    cy.contains("Login").click();
+
 
 
   });
