1a27085e334b7a4ab77f7ebfec1f67a3858c3d0d
GitLukeW <gitlukew@gmail.com>
2023-10-01 17:11:20 -0400

    chore(commit-line): added commit linting

---
+++ validate-branch-name.js
@@ -0,0 +1,11 @@
+const execSync = require('child_process').execSync;
+
+const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+const branchNamePattern = /^(feat|bugfix|hotfix|test|chore)\/issue-\d+\/[\w-]+$/;
+
+if (!branchNamePattern.test(branchName)) {
+    console.error(`ERROR: The branch name "${branchName}" does not follow the required format!`);
+    console.error('\nExpected format: <type>/issue-<number>/<description>');
+    console.error('Example: feat/issue-123/add-login-button\n');
+    process.exit(1);
+}
