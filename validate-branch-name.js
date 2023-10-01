63d13164236dd2115015d69f665b428b48c62147
GitLukeW <gitlukew@gmail.com>
2023-10-01 17:39:27 -0400

    chore(husky): setup-husky

--- validate-branch-name.js
+++ validate-branch-name.js
@@ -1,11 +1,16 @@
-const execSync = require('child_process').execSync;
+const execSync = require("child_process").execSync;
 
-const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-const branchNamePattern = /^(feat|bugfix|hotfix|test|chore)\/issue-\d+\/[\w-]+$/;
+const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+  .toString()
+  .trim();
+const branchNamePattern =
+  /^(feat|bugfix|hotfix|test|chore)\/issue-\d+\/[\w-]+$/;
 
 if (!branchNamePattern.test(branchName)) {
-    console.error(`ERROR: The branch name "${branchName}" does not follow the required format!`);
-    console.error('\nExpected format: <type>/issue-<number>/<description>');
-    console.error('Example: feat/issue-123/add-login-button\n');
-    process.exit(1);
+  console.error(
+    `ERROR: The branch name "${branchName}" does not follow the required format!`,
+  );
+  console.error("\nExpected format: <type>/issue-<number>/<description>");
+  console.error("Example: feat/issue-123/add-login-button\n");
+  process.exit(1);
 }
